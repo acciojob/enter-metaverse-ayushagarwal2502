b0ae4d38ea613bcfdab2e6de5ce9e129972e5e8b
ayushagarwal2502 <159728868+ayushagarwal2502@users.noreply.github.com>
2025-01-13 01:49:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required.
 //let count=document.getElementById("status").innerHTML
 function changeMatter(){
-  document.getElementById("status").innerHTML="Entered Metaverse"
+  document.getElementById("status").innerHTML="<h1>Entered Metaverse</h1>"
 
 }
 //changeMatter();  
